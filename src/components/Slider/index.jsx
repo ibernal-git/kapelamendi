9a10ef3ac81e28f221857cb7238eb5e424a49d1a
Imanol Bernal <info@ibernal.dev>
2022-02-28 22:47:31 +0100

    feat: add slider

---
+++ src/components/Slider/index.jsx
@@ -0,0 +1,13 @@
+import styles from './Slider.module.css'
+export default function Slider () {
+  return (
+    <>
+      <div className={styles.slider}>
+        <img className={styles.sliderImg} src='/images/slider_1.jpg' />
+        <img className={styles.sliderImg} src='/images/slider_1.jpg' />
+
+      </div>
+    </>
+
+  )
+}
