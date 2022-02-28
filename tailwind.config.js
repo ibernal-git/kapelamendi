2410b8ec5a6ce9fe368860d8fd83735157a41d3b
Imanol Bernal <info@ibernal.dev>
2022-02-28 22:45:22 +0100

    feat: add tailwind and config theme colors

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './public/**/*.html',
+    './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}'
+  ],
+  theme: {
+    colors: {
+      primary: '#fc9200',
+      secondary: '#062b5b',
+      accent: '#fff0db',
+      links: '#062b5b'
+    }
+  }
+  // more options here
+}
