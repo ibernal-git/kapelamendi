2f1c04e7617a619081fa278de57dc52d035ed9bb
Imanol Bernal <info@ibernal.dev>
2022-02-28 22:47:16 +0100

    feat: add navbar

---
+++ src/components/NavButton/index.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react'
+
+export default function MenuButton ({ link, isMobile, children }) {
+  const [pathname, setPathname] = useState('')
+
+  useEffect(() => {
+    setPathname(globalThis.document.location.pathname)
+  }, [])
+
+  const isCurrentPage = (link) => {
+    if (pathname === link) {
+      console.log('es true')
+      return true
+    }
+    return false
+  }
+  return (
+
+    <a href={link} className={`${isMobile ? 'block py-2' : ''} ${isCurrentPage(link) ? 'aria-current="page"' : ''}`}>{children}</a>
+
+  )
+}
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,52 @@
+import NavButton from '../NavButton'
+import { useState } from 'react'
+export default function Navbar ({ menuItems }) {
+  const [isHidden, setIsHidden] = useState(true)
+  const handleMenuIcon = () => {
+    setIsHidden(!isHidden)
+  }
+  return (
+    <div>
+      <nav className='bg-primary py-4 px-8 fixed w-full'>
+        <div className='max-w-7xl mx-auto'>
+          <div className='flex justify-between items-center'>
+            <div className='items-center text-lg font-bold'>Kapelamendi</div>
+            <div className='hidden lg:flex items-center space-x-4 text-md font-medium'>
+              {menuItems.map((item) => {
+                return <NavButton key={item.link} link={item.link}>{item.text}</NavButton>
+              })}
+            </div>
+            <div className='lg:hidden flex items-center'>
+              <button onClick={handleMenuIcon}>
+                {
+                  isHidden
+
+                    ? (
+                      <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6 lg:hidden' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+                        <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M4 6h16M4 12h16M4 18h16' />
+                      </svg>
+                      )
+                    : (
+                      <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6 lg:hidden' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+                        <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M6 18L18 6M6 6l12 12' />
+                      </svg>
+                      )
+                }
+
+              </button>
+
+            </div>
+
+          </div>
+        </div>
+        <div className={` ${isHidden ? 'hidden' : ''} lg:hidden text-md font-medium items-center text-center`}>
+          {menuItems.map((item) => {
+            return <NavButton key={item.link} link={item.link} isMobile>{item.text}</NavButton>
+          })}
+        </div>
+      </nav>
+
+    </div>
+
+  )
+}
